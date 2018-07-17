37349ba894ff2c76c573be0dcb6ca690921a503c
Michael Coleman <michaelcoleman19@gmail.com>
2018-07-17 14:20:07 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
-end
+  my_new_hash = {}
+  style_array = []
+    languages.each do |variable1, variable2|
+      style_array.push(variable1)
+      variable2.each do |variable3, variable4|
+        variable4.each do |variable5, variable6|
+          my_new_hash = {
+            variable3 => {
+              variable5 => languages[variable1][variable3][variable5],
+              :style => style_array
+              }
+            }
+        end
+      end
+    end
+    return my_new_hash
+  end
